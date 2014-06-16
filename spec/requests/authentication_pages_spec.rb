41d10fa3ecbc82dc38e569634b715dadc70b24b4
ribrciti <rbciti@msn.com>
2014-06-16 14:47:59 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe "Authentication" do
 
-  subject { page }
+    subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
 
   describe "signin" do
     before { visit signin_path }
@@ -11,17 +18,21 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-  	
-  	describe "with valid information" do
+
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
 
       it { should have_title(user.name) }
       it { should have_link('Users',       href: users_path) }
@@ -74,22 +85,15 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
-      end    
-  
-      describe "as wrong user" do
-        let(:user) { FactoryGirl.create(:user) }
-        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-        before { sign_in user, no_capybara: true }
 
-        describe "submitting a GET request to the Users#edit action" do
-          before { get edit_user_path(wrong_user) }
-          specify { expect(response.body).not_to match(full_title('Edit user')) }
-          specify { expect(response).to redirect_to(root_url) }
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
         end
 
-        describe "submitting a PATCH request to the Users#update action" do
-          before { patch user_path(wrong_user) }
-          specify { expect(response).to redirect_to(root_url) }
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
         end
       end
 
@@ -104,6 +108,35 @@ describe "Authentication" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
 
